2889d61f0f1a80a61cf57fc79f2ab69405ef6944
Acorn <polal92@mail.ru>
2025-07-04 15:27:56 +0300

    reset

---
+++ p4/main.py
@@ -0,0 +1,58 @@
+import numpy as np
+import sympy as sp
+import matplotlib.pyplot as plt
+#
+# к Видео 4.2
+# задача 1
+#
+def v4_2():
+    t = sp.symbols('t')
+    O = sp.Matrix([5*t-1, 4*t**3 +1])
+
+    sp.pprint(O)
+
+    V = sp.diff(O, t)
+    a = sp.diff(V, t)
+
+    sp.pprint(V)
+    sp.pprint(a)
+
+    at = a.dot(V)/V.norm() * V/V.norm()
+    sp.pprint(at)
+
+    an = (a - at).norm()
+    sp.pprint(an)
+
+    an_v = []
+
+    for i in range(0, 6):
+        an_v.append(an.subs(t,i).evalf())
+
+    print(max(an_v))
+
+#
+# Видео 4.3
+# Задача 3
+#
+def v4_3():
+    t, r2, R2, r3, R3 = sp.symbols('t r2 R2 r3 R3')
+    # t = np.linspace(-10, 10, 100)
+    xt = 2 + 70*t**2
+    x0 = xt.subs(t, 0)
+    sp.pprint(x0)
+    w1 = sp.diff(xt)
+    sp.pprint(w1)
+    wr2 = w1/r2
+    sp.pprint(wr2)
+    vR2 = wr2*R2
+    sp.pprint(vR2)
+    wR3 = vR2/R3
+    sp.pprint(wR3)
+    vM = wR3*r3
+    sp.pprint(vM)
+
+def main():
+    v4_3()
+
+if __name__ == "__main__":
+    main()

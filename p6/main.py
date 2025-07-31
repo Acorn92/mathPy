9c76a248397ed917937119d74dc05566e822936e
alx <polal92@mail.ru>
2025-07-31 13:08:33 +0300

    Выполнен тест 6_3

--- p6/main.py
+++ p6/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy.stats import *
 
 def w6_1():
     def F(x):
@@ -13,8 +13,17 @@ def w6_1():
 
     print(F(1) - F(1/4))
 
+def w6_3():
+    paret = pareto(b=2.63)
+    fig, ax = plt.subplots(1, 2)
+
+    x=np.linspace(0, 10, 100)
+    ax[0].plot(x, paret.pdf(x), lw=5, alpha=0.6)
+    ax[1].plot(x, paret.cdf(x), lw=5, alpha=0.6)
+    plt.show()
+
 def main():
-    w6_1()
+    w6_3()
 
 if __name__ == "__main__":
     main()

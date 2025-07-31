e8658c29e279cc48fc973ec755c0c57c17975abe
Acorn <polal92@mail.ru>
2025-07-31 10:15:29 +0300

    Все кроме последней задачи

--- p6/main.py
+++ p6/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy.stats import expon
+from scipy.integrate import dblquad
 
 def w6_1():
     def F(x):
@@ -13,8 +14,23 @@ def w6_1():
 
     print(F(1) - F(1/4))
 
+def w6_4():
+    e = expon(scale = 4)
+    print(e.var())
+
+def w6_5():
+  
+    def func(x, y):
+        C = np.array([0, 0])
+        A = np.array([0, 4])
+        B = np.array([4, 0])
+        return (A + x * (B - A) + y * (C-A))[0]
+    
+    # print(func(0,4))
+    print(dblquad(func, 0, 4, lambda x: 0, lambda x: 4 - x))
+
 def main():
-    w6_1()
+    w6_5()
 
 if __name__ == "__main__":
     main()
